7e042c1ec6aa3f74b1bf5de37ca3187d97dcf90c
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-13 14:52:32 +0000

    finished method factors

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 /**
- *
- * @author 
+ * finds factors of a number with methods
+ * @jacoe0850 
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+// this line creates a procedure method with the method name factors and the paramater is whatever number they input
+  public static void factors(int num){
+    // for loop because i know that it will loop 30 times and it finds all factors of 30
+  for(int i = 1; i < num; i++){
+    if(num % i == 0){
+      System.out.println(i);
+    }
+  }
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+factors(10000000);
   }
 }
